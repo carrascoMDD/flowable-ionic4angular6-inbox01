668d5318a96da76d6a46664ef54185c6a38f7d0e
carrascoMDD <carrascomdd@gmail.com>
2018-06-01 04:54:47 +0200

    More ... menu in About now points to workOflow github repo

--- flowable-ionic4angular6-inbox01app/src/pages/about-popover/about-popover.ts
+++ flowable-ionic4angular6-inbox01app/src/pages/about-popover/about-popover.ts
@@ -38,10 +38,7 @@ import { App, NavController, ModalController, ViewController } from 'ionic-angul
 @Component({
   template: `
     <ion-list>
-      <button ion-item (click)="close('http://ionicframework.com/docs/v2/getting-started')">Learn Ionic</button>
-      <button ion-item (click)="close('http://ionicframework.com/docs/v2')">Documentation</button>
-      <button ion-item (click)="close('http://showcase.ionicframework.com')">Showcase</button>
-      <button ion-item (click)="close('https://github.com/ionic-team/ionic')">GitHub Repo</button>
+      <button ion-item (click)="close('https://github.com/carrascoMDD/flowable-ionic4angular6-inbox01')">GitHub Repo</button>
       <button ion-item (click)="support()">Support</button>
     </ion-list>
   `
